06045287d8d63e3ce317c43cb7172a1e65a1a20f
sravyakota <crazysravs577@gmail.com>
2022-09-09 14:41:01 +0000

    destination search modified

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,47 +1,53 @@
+import {Component} from 'react'
+
 import Items from '../DestinationItem'
 
 import './index.css'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
 
-  const onSearch = event => {
-    const input = event.target.value
-
-    const searchResults = destinationsList.filter(eachUser =>
-      eachUser.name.includes(input),
-    )
-    return searchResults
+  onSearch = event => {
+    this.setState({searchInput: event.target.value})
   }
 
-  return (
-    <div className="bg">
-      <h1 className="heading">Destination Search</h1>
-      <div className="searchDiv">
-        <input
-          type="search"
-          placeholder="search"
-          className="searchBox"
-          onChange={onSearch}
-        />
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
-          alt="search icon"
-          className="icon"
-        />
-      </div>
-      <ul className="list">
-        {destinationsList.map(eachUser => (
-          <Items
-            imgUrl={eachUser.imgUrl}
-            name={eachUser.name}
-            key={eachUser.id}
+  render() {
+    const {destinationsList} = this.props
+    const {searchInput} = this.state
+
+    const searchResults = destinationsList.filter(eachUser =>
+      eachUser.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="bg">
+        <h1 className="heading">Destination Search</h1>
+        <div className="searchDiv">
+          <input
+            type="search"
+            placeholder="search"
+            className="searchBox"
+            onChange={this.onSearch}
           />
-        ))}
-        )
-      </ul>
-    </div>
-  )
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+            alt="search icon"
+            className="icon"
+          />
+        </div>
+        <ul className="list">
+          {searchResults.map(eachUser => (
+            <Items
+              imgUrl={eachUser.imgUrl}
+              name={eachUser.name}
+              key={eachUser.id}
+            />
+          ))}
+          )
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default DestinationSearch
